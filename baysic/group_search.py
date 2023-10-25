25cc8a07a3f01fd11d452a6240f59b4e50c4d290
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2023-10-25 03:18:29 -0400

    pain

--- baysic/group_search.py
+++ baysic/group_search.py
@@ -148,10 +148,9 @@ def main_(conf: MainConfig):
 
     if conf.log.use_directory:
         date = datetime.now().strftime('%m-%d')
-        date_dir = conf.log.log_directory / date
-        date_dir = date_dir / Path(conf.target.formula)
+        date_dir = conf.log.log_directory / Path(conf.target.formula)
         if not date_dir.exists():
-            date_dir.mkdir(parents=True)
+            date_dir.mkdir()
 
         run_num = 1
         while (date_dir / str(run_num)).exists() and conf.log.log_dir_mode == FileLoggingMode.new:
